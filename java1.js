d382c4d1ee025f85974e0dd672d622133ef82796
Marynosik <88712648+Marynosik@users.noreply.github.com>
2022-09-15 09:34:06 +0200

    Add files via upload

--- java1.js
+++ java1.js
@@ -3,5 +3,24 @@ const form = document.getElementById('form')
 form.addEventListener('submit', (event) => {
     event.preventDefault();
     //validation
-    form.submit();
+  const f = Object.values(form);
+
+  for(const input of f) {
+    console.log(`Input ${input.id} has value ${input.value}`);
+
+    switch (input.id){
+        case 'email':
+            break;
+            case 'name':
+                break;
+                case 'body':
+                    break;
+                    case 'agree':
+                    break;
+                    default:
+                        break;
+    }
+
+
+}
 })
